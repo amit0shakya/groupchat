df496564a97b60be10d24a6dfd4263b504bd41c6
amit shakya <amit.shakya@webitude.com>
2018-01-26 21:49:52 +0530

    server update

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 var port = process.env.PORT || 3000;
-var http = require('http').Server(app)
-var io = require('socket.io')(http);
-var bodyParser = require('body-parser')
+//var http = require('http').Server(app)
+//var io = require('socket.io')(http);
+//var bodyParser = require('body-parser')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,39 +13,14 @@ app.use(bodyParser.json());
 
 app.engine('html',require('ejs').renderFile);
 
-app.set('views','./client')
+//app.set('views','./client')
 
 app.use('/js', express.static(path.join(__dirname, './lib/js')))
 app.use('/img', express.static(path.join(__dirname, './lib/images')))
 
 app.get('/',function(req,res){
-	res.render("index.html");
-})
-
-var totalClients=0;
-var users = [];
-
-io.on('connection',function(socket){
-
-	console.log('user connected')
-
-	socket.on('join',function(data){
-		totalClients++;
-		console.log(data,"<<<username")
-		users.push(data);
-	})
-
-	socket.on('userchat',function(data){
-		console.log(data,"<<<Data")
-		//socket.emit('servermsg', data);
-		io.sockets.emit('servermsg', data);
-	})
-
-	socket.on('disconnect',function(){
-		totalClients--;
-		console.log('user disconnect');
-	})
-
+	//res.render("index.html");
+    res.send("hello world")
 })
 
 http.listen(port,function(req,res){

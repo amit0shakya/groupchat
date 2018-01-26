c41c94191f0337839d7ca77479e9098ff926b260
amit shakya <amit.shakya@webitude.com>
2018-01-26 21:53:46 +0530

    server update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 //var http = require('http').Server(app)
 //var io = require('socket.io')(http);
-//var bodyParser = require('body-parser')
+var bodyParser = require('body-parser')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,17 +15,23 @@ app.engine('html',require('ejs').renderFile);
 
 //app.set('views','./client')
 
-app.use('/js', express.static(path.join(__dirname, './lib/js')))
-app.use('/img', express.static(path.join(__dirname, './lib/images')))
+//app.use('/js', express.static(path.join(__dirname, './lib/js')))
+//app.use('/img', express.static(path.join(__dirname, './lib/images')))
 
 app.get('/',function(req,res){
 	//res.render("index.html");
     res.send("hello world")
 })
 
+app.listen(port,function(req,res){
+    console.log("app listening on port "+port);
+})
+
+/*
 http.listen(port,function(req,res){
 	console.log("app ruuning on PORT "+port)
 })
+*/
 
 app.use(function(req,res){
 	res.send('404');
